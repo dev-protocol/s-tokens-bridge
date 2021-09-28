9b83a054075604990ab8eb06ac7e11164c750af2
CryptoDogg <d.enkikairo@gmail.com>
2021-09-28 13:25:29 +0900

    undo config file change

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,12 +7,4 @@ import '@nomiclabs/hardhat-waffle'
 
 module.exports = {
         solidity: "0.8.4",
-        networks: {
-                hardhat: {
-                        gas: 12000000,
-                        blockGasLimit: 0x1fffffffffffff,
-                        allowUnlimitedContractSize: true,
-                        timeout: 1800000
-                }
-        }
 };
