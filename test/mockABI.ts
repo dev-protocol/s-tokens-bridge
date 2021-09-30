dd15b8752e5f0509141cac7cbb38e2271269bbc7
CryptoDogg <d.enkikairo@gmail.com>
2021-09-30 13:28:20 +0900

    add BridgeL2

--- test/mockABI.ts
+++ test/mockABI.ts
@@ -62,3 +62,75 @@ export const mockSTokensManagerABI = [
 		type: 'function',
 	},
 ]
+
+export const mockSTokensManagerL2ABI = [
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'from',
+				type: 'address',
+			},
+			{
+				internalType: 'address',
+				name: 'to',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'tokenId',
+				type: 'uint256',
+			},
+		],
+		name: 'transferFrom',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'uint256',
+				name: '_tokenId',
+				type: 'uint256',
+			},
+		],
+		name: 'positions',
+		outputs: [
+			{
+				components: [
+					{
+						internalType: 'address',
+						name: 'property',
+						type: 'address',
+					},
+					{
+						internalType: 'uint256',
+						name: 'amount',
+						type: 'uint256',
+					},
+					{
+						internalType: 'uint256',
+						name: 'price',
+						type: 'uint256',
+					},
+					{
+						internalType: 'uint256',
+						name: 'cumulativeReward',
+						type: 'uint256',
+					},
+					{
+						internalType: 'uint256',
+						name: 'pendingReward',
+						type: 'uint256',
+					},
+				],
+				internalType: "struct ISTokensManager.StakingPositions",
+				name: "",
+				type: "tuple"
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+]
